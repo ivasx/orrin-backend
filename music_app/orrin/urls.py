b81933dd523ad5afcac3bf654c60c850e3653c68
ivasx <ambroziak.v.ivan@gmail.com>
2025-09-16 18:50:03 +0300

    Authentication: - implemented logout functionality

--- music_app/orrin/urls.py
+++ music_app/orrin/urls.py
@@ -1,7 +1,15 @@
+from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 from django.urls import path
+
+from music_app import settings
 from . import views
 from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
